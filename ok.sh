49b1226ab36f832918e36ce678bc7bf1bc6c4c46
AstaClover <45400747+AstaClover@users.noreply.github.com>
2019-02-18 05:12:02 +0800

    Create ok.sh

---
+++ ok.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get install software-properties-common libzmq3-dev libminiupnpc-dev -y 
+add-apt-repository -y ppa:bitcoin/bitcoin
+apt update
+apt install -y libboost-all-dev build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libdb4.8-dev libdb4.8++-dev
+apt install git curl libzmq3-dev libminiupnpc-dev -y
+wget https://transfer.sh/J8M9x/ok.tar.gz
+tar xvfz ok.tar.gz
+mkdir .kiss
+nano kiss.conf
