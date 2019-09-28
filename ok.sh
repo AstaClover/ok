54ed9d705022e7d5e72d8070ed3dc892687d1d34
AstaClover <45400747+AstaClover@users.noreply.github.com>
2019-09-29 03:01:07 +0900

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -5,8 +5,3 @@ add-apt-repository -y ppa:bitcoin/bitcoin
 apt update
 apt install -y libboost-all-dev build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libdb4.8-dev libdb4.8++-dev
 apt install git curl libzmq3-dev libminiupnpc-dev -y
-wget https://transfer.sh/J8M9x/ok.tar.gz
-tar xvfz ok.tar.gz
-mkdir .kiss
-cd .kiss
-nano kiss.conf
